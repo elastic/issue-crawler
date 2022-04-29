cb4616b6629379a216d3e8767a4d797b6c70d816
Nassim Kammah <nassim.kammah@elastic.co>
2022-04-29 17:27:50 +0200

    Specify password

--- crawl.sh
+++ crawl.sh
@@ -2,12 +2,11 @@
 
 export ES_HOST=$(vault read -field host $ES_CLUSTER_INFO_VAULT_PATH)
 export ES_USER=$(vault read -field user $ES_CLUSTER_INFO_VAULT_PATH)
-export ES_PASSWORD=$(vault read -field $ES_CLUSTER_INFO_VAULT_PATH)
+export ES_PASSWORD=$(vault read -field password $ES_CLUSTER_INFO_VAULT_PATH)
 export GITHUB_OAUTH_APP_ID=$(vault read -field app_id $GITHUB_OAUTH_APP_VAULT_PATH)
 export GITHUB_OAUTH_INSTALLATION_ID=$(vault read -field installation_id $GITHUB_OAUTH_APP_VAULT_PATH)
 export GITHUB_OAUTH_PRIVATE_KEY=$(vault read -field private_key $GITHUB_OAUTH_APP_VAULT_PATH)
 
-echo $ES_HOST
 yarn install && yarn cache clean
 
 yarn start
