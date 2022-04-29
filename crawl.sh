5164baad78d762e81857114a6b1eb1a9a42b8e61
Nassim Kammah <nassim.kammah@elastic.co>
2022-04-29 17:22:05 +0200

    Trying out another approach

--- crawl.sh
+++ crawl.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-echo $SCRIPTPATH
-cd $SCRIPTPATH
+export ES_HOST=$(vault read -field host $ES_CLUSTER_INFO_VAULT_PATH)
+export ES_USER=$(vault read -field user $ES_CLUSTER_INFO_VAULT_PATH)
+export ES_PASSWORD=$(vault read -field $ES_CLUSTER_INFO_VAULT_PATH)
+export GITHUB_OAUTH_APP_ID=$(vault read -field app_id $GITHUB_OAUTH_APP_VAULT_PATH)
+export GITHUB_OAUTH_INSTALLATION_ID=$(vault read -field installation_id $GITHUB_OAUTH_APP_VAULT_PATH)
+export GITHUB_OAUTH_PRIVATE_KEY=$(vault read -field private_key $GITHUB_OAUTH_APP_VAULT_PATH)
 
-pwd
-
-export ES_HOST=$(vault read -field host secret/ci/elastic-issue-crawler/github-stats-user)
-export ES_USER=$(vault read -field user secret/ci/elastic-issue-crawler/github-stats-user)
-export ES_PASSWORD=$(vault read -field password secret/ci/elastic-issue-crawler/github-stats-user)
-export GITHUB_OAUTH_APP_ID=$(vault read -field app_id secret/ci/elastic-issue-crawler/github_oauth_app)
-export GITHUB_OAUTH_INSTALLATION_ID=$(vault read -field installation_id secret/ci/elastic-issue-crawler/github_oauth_app)
-export GITHUB_OAUTH_PRIVATE_KEY=$(vault read -field private_key secret/ci/elastic-issue-crawler/github_oauth_app)
+RUN yarn install && \
+    yarn cache clean
 
 yarn start
