8a8df3ab5058fd37c4330f49d973a9fedb99c630
Nassim Kammah <nassim.kammah@elastic.co>
2022-04-29 14:10:15 +0200

    Stop using the environment hook to get around the bash issue

--- .buildkite/crawl.sh
+++ .buildkite/crawl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo $PRIVATE_REPOS
+# The GH repo is not needed since the github-issue crawler code is contained within the issue-crawler image
+export BUILDKITE_REPO=""
 
 ES_HOST=$(vault read -field host secret/ci/elastic-issue-crawler/github-stats-user)
 ES_USER=$(vault read -field user secret/ci/elastic-issue-crawler/github-stats-user)
@@ -8,5 +9,8 @@ ES_PASSWORD=$(vault read -field password secret/ci/elastic-issue-crawler/github-
 GITHUB_OAUTH_APP_ID=$(vault read -field app_id secret/ci/elastic-issue-crawler/github_oauth_app)
 GITHUB_OAUTH_INSTALLATION_ID=$(vault read -field installation_id secret/ci/elastic-issue-crawler/github_oauth_app)
 GITHUB_OAUTH_PRIVATE_KEY=$(vault read -field private_key secret/ci/elastic-issue-crawler/github_oauth_app)
+
+echo $ES_HOST
+echo $PRIVATE_REPOS
 
 yarn start
