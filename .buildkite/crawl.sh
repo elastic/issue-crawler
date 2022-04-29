5c0ccf3926dbba97d90cbe71058ce0926d6039de
Nassim Kammah <nassim.kammah@elastic.co>
2022-04-29 14:00:18 +0200

    Add logic and vault access

--- .buildkite/crawl.sh
+++ .buildkite/crawl.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "Hello, World."
+echo $PRIVATE_REPOS
+
+ES_HOST=$(vault read -field host secret/ci/elastic-issue-crawler/github-stats-user)
+ES_USER=$(vault read -field user secret/ci/elastic-issue-crawler/github-stats-user)
+ES_PASSWORD=$(vault read -field password secret/ci/elastic-issue-crawler/github-stats-user)
+GITHUB_OAUTH_APP_ID=$(vault read -field app_id secret/ci/elastic-issue-crawler/github_oauth_app)
+GITHUB_OAUTH_INSTALLATION_ID=$(vault read -field installation_id secret/ci/elastic-issue-crawler/github_oauth_app)
+GITHUB_OAUTH_PRIVATE_KEY=$(vault read -field private_key secret/ci/elastic-issue-crawler/github_oauth_app)
+
+yarn start
