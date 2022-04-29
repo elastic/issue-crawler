5f520545bd639a99eccc2a0e38f481954e7d6eed
Nassim Kammah <nassim.kammah@elastic.co>
2022-04-29 15:19:01 +0200

    export the variables to the env

--- crawl.sh
+++ crawl.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ES_HOST=$(vault read -field host secret/ci/elastic-issue-crawler/github-stats-user)
-ES_USER=$(vault read -field user secret/ci/elastic-issue-crawler/github-stats-user)
-ES_PASSWORD=$(vault read -field password secret/ci/elastic-issue-crawler/github-stats-user)
-GITHUB_OAUTH_APP_ID=$(vault read -field app_id secret/ci/elastic-issue-crawler/github_oauth_app)
-GITHUB_OAUTH_INSTALLATION_ID=$(vault read -field installation_id secret/ci/elastic-issue-crawler/github_oauth_app)
-GITHUB_OAUTH_PRIVATE_KEY=$(vault read -field private_key secret/ci/elastic-issue-crawler/github_oauth_app)
+export ES_HOST=$(vault read -field host secret/ci/elastic-issue-crawler/github-stats-user)
+export ES_USER=$(vault read -field user secret/ci/elastic-issue-crawler/github-stats-user)
+export ES_PASSWORD=$(vault read -field password secret/ci/elastic-issue-crawler/github-stats-user)
+export GITHUB_OAUTH_APP_ID=$(vault read -field app_id secret/ci/elastic-issue-crawler/github_oauth_app)
+export GITHUB_OAUTH_INSTALLATION_ID=$(vault read -field installation_id secret/ci/elastic-issue-crawler/github_oauth_app)
+export GITHUB_OAUTH_PRIVATE_KEY=$(vault read -field private_key secret/ci/elastic-issue-crawler/github_oauth_app)
 
 echo $ES_HOST
 echo $PRIVATE_REPOS
